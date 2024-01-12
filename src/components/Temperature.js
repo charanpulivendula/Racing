ee71b9abf197c76e7c9bd9b366567ed47eed61e0
tssanj <tssanj@iu.edu>
2024-01-12 00:47:12 -0500

    Revert "UI chnages"
    This reverts commit 18da674d38a5769d5aa270ac2504931e73f71467.

--- src/components/Temperature.js
+++ src/components/Temperature.js
@@ -1,37 +1,10 @@
-import { useState,useEffect } from 'react';
+import { useState } from "react";
 import car_temp from '../Assets/car_temp.png';
-import steering_img from '../Assets/steering2.png';
 import './Temperature.css'
 const Temperature = ()=>{
     const [temperature,setTemperature] = useState([36,36,36,36]);
-    const [steering,setSteering] = useState(0);
-    useEffect(() => {
-        const getRandomAngle = () => Math.floor(Math.random() * 361) - 180;
-    
-        const interval = setInterval(() => {
-          const randomAngle = getRandomAngle();
-          setSteering(randomAngle);
-        }, 1000);
-    
-        return () => clearInterval(interval);
-      }, []);
     return (
         <div className="temp w-1/4 flex place-items-end ">
-            <div className="steering h-1/4 flex">
-                <div className="steering-image w-1/2 flex justify-start m-2">
-                <img
-                    src={steering_img}
-                    width={80}
-                    alt="steering"
-                    // style={{ transform: `rotate(${steering}deg)`,
-                    // transition: 'transform 0.5s ease' }}
-                />
-                </div>
-                <div className='steering-val flex place-items-center text-2xl'>
-                    {steering}
-                </div>
-            </div>
-            <div>
             <div className="temp-box flex-col w-1/4 mt-5">
                 <div className="temp-val flex justify-end mb-32">
                     {temperature[0]}&deg;C
@@ -50,7 +23,6 @@ const Temperature = ()=>{
                 <div className="temp-val flex justify-start">
                     {temperature[3]}&deg;C
                 </div>
-            </div>
             </div>
         </div>
     );
